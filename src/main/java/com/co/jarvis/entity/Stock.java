ce937abeff762353658513de631f1807198440aa
JoseFelix88 <josecuriel88@gmail.com>
2025-10-19 21:48:14 -0500

    feat: add NoArgsConstructor annotation to Stock entity class

--- src/main/java/com/co/jarvis/entity/Stock.java
+++ src/main/java/com/co/jarvis/entity/Stock.java
@@ -4,11 +4,13 @@ import com.co.jarvis.enums.UnitMeasure;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Stock {
     private BigDecimal quantity;
